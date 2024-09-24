baca7a5a25acb74ff8823ebdd8b65016da0c40a8
CentOS_API <19805318060@139.com>
2024-09-24 21:42:02 +0800

    完善登录功能
    现在，用户登录之后，会在本地存储空间中存储Token，之后每次进入根路由之后，都会判断是否已登录，已经登录则进入首页，否则跳转至登录页

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,28 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../views/Login.vue';
+import Home from '../views/Home.vue';
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/',
+    redirect: '/login' // 默认重定向到登录页面
+  }
+];
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: []
-})
+  history: createWebHistory(),
+  routes
+});
 
-export default router
+export default router;
+
